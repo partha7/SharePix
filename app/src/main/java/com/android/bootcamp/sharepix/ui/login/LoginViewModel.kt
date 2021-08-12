2663a105ba652303bcb83594f270c7d88f5a155b
partha7 <parthashum@gmail.com>
2021-08-12 15:40:15 +0530

    Adding login featue

---
+++ app/src/main/java/com/android/bootcamp/sharepix/ui/login/LoginViewModel.kt
@@ -0,0 +1,73 @@
+package com.android.bootcamp.sharepix.ui.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.android.bootcamp.sharepix.data.repository.UserRepository
+import com.android.bootcamp.sharepix.ui.base.BaseViewModel
+import com.android.bootcamp.sharepix.utils.common.*
+import com.android.bootcamp.sharepix.utils.network.NetworkHelper
+import com.android.bootcamp.sharepix.utils.rx.SchedulerProvider
+import io.reactivex.disposables.CompositeDisposable
+
+class LoginViewModel(
+    schedulerProvider: SchedulerProvider,
+    compositeDisposable: CompositeDisposable,
+    networkHelper: NetworkHelper,
+    private val userRepository: UserRepository
+) : BaseViewModel(schedulerProvider, compositeDisposable, networkHelper) {
+
+    private val validationsList: MutableLiveData<List<Validation>> = MutableLiveData()
+
+    val launchMain: MutableLiveData<Event<Map<String, String>>> = MutableLiveData()
+
+    val emailField: MutableLiveData<String> = MutableLiveData()
+    val passwordField: MutableLiveData<String> = MutableLiveData()
+    val loggingIn: MutableLiveData<Boolean> = MutableLiveData()
+
+    val emailValidation: LiveData<Resource<Int>> = filterValidation(Validation.Field.EMAIL)
+    val passwordValidation: LiveData<Resource<Int>> = filterValidation(Validation.Field.PASSWORD)
+
+    private fun filterValidation(field: Validation.Field) =
+        Transformations.map(validationsList) {
+            it.find { validation -> validation.field == field }
+                ?.run { return@run this.resource }
+                ?: Resource.unknown()
+        }
+
+    override fun onCreate() {}
+
+    fun onEmailChange(email: String) = emailField.postValue(email)
+
+    fun onPasswordChange(email: String) = passwordField.postValue(email)
+
+    fun onLogin() {
+        val email = emailField.value
+        val password = passwordField.value
+
+        val validations = Validator.validateLoginFields(email, password)
+        validationsList.postValue(validations)
+
+        if (validations.isNotEmpty() && email != null && password != null) {
+            val successValidation = validations.filter { it.resource.status == Status.SUCCESS }
+            if (successValidation.size == validations.size && checkInternetConnectionWithMessage()) {
+                loggingIn.postValue(true)
+                compositeDisposable.addAll(
+                    userRepository.doUserLogin(email, password)
+                        .subscribeOn(schedulerProvider.io())
+                        .subscribe(
+                            {
+                                userRepository.saveCurrentUser(it)
+                                loggingIn.postValue(false)
+                                launchMain.postValue(Event(emptyMap()))
+                            },
+                            {
+                                handleNetworkError(it)
+                                loggingIn.postValue(false)
+                            }
+                        )
+                )
+            }
+        }
+    }
+}
